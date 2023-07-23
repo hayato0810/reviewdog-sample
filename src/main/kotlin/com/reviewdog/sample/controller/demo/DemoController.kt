bd18d4fd4ca4ed0536d7d795c002b9110d3756a2
hayato <31178172+hayato0810@users.noreply.github.com>
2023-07-24 00:44:39 +0900

    add comment

--- src/main/kotlin/com/reviewdog/sample/controller/demo/DemoController.kt
+++ src/main/kotlin/com/reviewdog/sample/controller/demo/DemoController.kt
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/reviewdog")
 class DemoController {
 
+    /*
+      * This comment is not formatted well.
+    */
     @GetMapping("/get")
     fun get(): String {
         return "Hello"
