6d26f42d52db053e6eb59c99f47b65de8a4e4d11
hayato <31178172+hayato0810@users.noreply.github.com>
2023-07-23 21:44:39 +0900

    add sprint project

---
+++ src/test/kotlin/com/reviewdog/sample/SampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.reviewdog.sample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
